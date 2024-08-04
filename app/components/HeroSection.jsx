a12299e8b0b7403a36cd72150987fc5fa455733a
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-04 19:05:47 -0300

    contrast fixes

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,9 +4,12 @@ export default function HeroSection() {
     return (
       <section className="hero min-h-screen flex items-center justify-center text-center px-4">
         <div>
-          <h1 className="text-5xl font-bold mb-4 text-primary">Monominimal</h1>
-          <p className="text-lg max-w-xl mx-auto text-primary">
-            Explorando o mundo do minimalismo e da arte, compartilhando ideias e inspirações para uma vida mais simples e significativa.
+          <h1 className="text-5xl font-bold mb-4 text-white text-contrast-filter text-contrast">
+            Monominimal
+          </h1>
+          <p className="text-lg max-w-xl mx-auto text-white text-contrast">
+            Explorando o mundo do minimalismo e da arte, compartilhando ideias e
+            inspirações para uma vida mais simples e significativa.
           </p>
         </div>
       </section>
