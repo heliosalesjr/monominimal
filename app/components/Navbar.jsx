20dc1e0f0ea3074820e9329793dd5236a0293825
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-04 21:33:24 -0300

    hero & nav adjustments

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,28 +1,28 @@
 "use client";
 
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu} from "@nextui-org/react";
-import {ChevronDown, Lock, Activity, Flash, Server, TagUser, Scale} from "./Icons.jsx";
-import {AcmeLogo} from "./AcmeLogo.jsx";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+
 
 export default function App() {
   
 
   return (
     <Navbar>
-      <NavbarBrand>
+      <NavbarBrand className="navbar-brand">
         
-        <p className="font-bold">Monominimal</p>
+        <h1 className="font-bold text-slate-800 hover:text-primary">Monominimal</h1>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         
         <NavbarItem isActive>
-          <Link href="#" aria-current="page">
+          <Link href="#" aria-current="page" className="hover:underline underline-offset-4 decoration-pink-700">
             Posts
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="hover:underline underline-offset-4 decoration-pink-700">
             About
           </Link>
         </NavbarItem>
